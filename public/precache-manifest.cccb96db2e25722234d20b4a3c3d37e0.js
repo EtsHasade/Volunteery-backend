9fa2776e08dc7c523d45529980e6ca220416887d
Elhanan Avihail <etsavihail@gmail.com>
2022-09-12 08:40:04 +0300

    init

---
+++ public/precache-manifest.cccb96db2e25722234d20b4a3c3d37e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2508e1746f9c6ffadfa",
+    "url": "/css/app.58dcf6a9.css"
+  },
+  {
+    "revision": "7097b2170050dd89bfea",
+    "url": "/css/chunk-vendors.84bb20f7.css"
+  },
+  {
+    "revision": "25ba4ea71ae01575b854681c42c1d0ef",
+    "url": "/fonts/Hind-Bold.25ba4ea7.ttf"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/fonts/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d72aa9755bd4ea07ba2ce83c29131cc7",
+    "url": "/img/hero.d72aa975.jpg"
+  },
+  {
+    "revision": "0c2bb58d383af72a48c6d2860f00fa4e",
+    "url": "/img/logo2.0c2bb58d.svg"
+  },
+  {
+    "revision": "2541c035b8aa4de544a99a5df5fa2eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e2508e1746f9c6ffadfa",
+    "url": "/js/app.4f43b905.js"
+  },
+  {
+    "revision": "7097b2170050dd89bfea",
+    "url": "/js/chunk-vendors.39a15d41.js"
+  },
+  {
+    "revision": "d35791b373e926343e96e9e527b3eec8",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ public/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.cccb96db2e25722234d20b4a3c3d37e0.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "volunteery"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
